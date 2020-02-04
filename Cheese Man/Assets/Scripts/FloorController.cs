3f3d04c2c42d947c63f7bb4d929b4373af798488
JesusACoria <48449114+JesusACoria@users.noreply.github.com>
2020-02-03 21:05:26 -0800

    made single block spawner

---
+++ Cheese Man/Assets/Scripts/FloorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour
+{
+    public float speed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FloorMovement();
+    }
+
+    private void FloorMovement()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Cheese Man/Assets/Scripts/SceneController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public float waitSeconds;
+    public Vector3 screenBounds;
+    public FloorController floorPrefab;
+
+    private Coroutine floorCoroutine;
+
+    // Use this for initialization
+    void Start()
+    {
+        screenBounds = GetScreenBounds();
+        floorCoroutine = StartCoroutine(ToddMcFarlaner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GetScreenBounds()
+    {
+        Camera mainCamera = Camera.main;
+        Vector3 screenVector = new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z);
+
+        return mainCamera.ScreenToWorldPoint(screenVector);
+    }
+
+    private IEnumerator ToddMcFarlaner()
+    {
+        WaitForSeconds wait = new WaitForSeconds(waitSeconds);
+
+        while (true)
+        {
+            float verticalRandomPosition = UnityEngine.Random.Range(-screenBounds.y, screenBounds.y);
+            Vector2 AlSimmonsLocation = new Vector2(screenBounds.x, verticalRandomPosition);
+
+            Instantiate(floorPrefab, AlSimmonsLocation, Quaternion.identity);
+
+            yield return wait;
+        }
+    }
+}
